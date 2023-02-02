5e4927d0df4dbb93b655bb86c9223f8d2fd9429d
Maksim Strelchenko <bender404123@gmail.com>
2023-02-03 00:31:29 +0300

    add output phone numbers to json. Changed regex

--- phone_numbers.py
+++ phone_numbers.py
@@ -8,11 +8,16 @@ If there is any, numbers will be written
 in 'founded_numbers.txt'
 """
 
+import json
 import re
 import urllib.request
 
 opener = urllib.request.FancyURLopener({})
 FOUNDED_PHONES = set()
+WEBSITES = {}
+
+# regex = "(8(\s?|-?)(\(?|-?)[4-9]\d{0,3}(\)?|-?)(\s?|-?)\d{3}(-?|\s?)\d{2}(-?|\s?)\d{2})"
+# regex_old = "(8\s?\(?[4-9]\d{0,3}\)?\s?\d{3}-?\d{2}-?\d{2})"
 
 with open('urls.txt', 'r') as urls_fi:
     data = urls_fi.readlines()
@@ -20,13 +25,24 @@ with open('urls.txt', 'r') as urls_fi:
 for i_url in data:
     f = opener.open(i_url.replace('\n', ''))
     content = f.read()
-    phone_numbers = set(re.findall(rb'(8\s?\(?[4-9]\d{0,3}\)?\s?\d{3}-?\d{2}-?\d{2})', content))
+    phone_numbers = list(set(re.findall(rb'(8\s?-?\(?[4-9]\d{0,3}\)?-?\s?-?\d{3}\s?-?\d{2}\s?-?\d{2})', content)))
     if phone_numbers:
-        FOUNDED_PHONES.update(phone_numbers)
+        filtered_phone_numbers = [
+            i_number.decode() for i_number in phone_numbers if len(
+                i_number.decode().replace('(', '').replace(')', '').replace('-', '').replace(' ', '')
+            ) == 11
+        ]
+        WEBSITES[i_url.replace('\n', '')] = filtered_phone_numbers
+        FOUNDED_PHONES.update(filtered_phone_numbers)
 
 with open('founded_numbers.txt', 'w') as fi:
     if FOUNDED_PHONES:
         for i_phone_number in FOUNDED_PHONES:
-            fi.write(i_phone_number.decode() + '\n')
+            fi.write(i_phone_number + '\n')
     else:
         fi.write('No phones founded.')
+
+if WEBSITES:
+    with open('phone_numbers.json', 'w') as fi:
+        json_data = json.dumps(WEBSITES, indent=4)
+        fi.write(json_data)
